94cd96524a49886de9ef8070d0e3c951d5014ed1
GiuseppeMota <giuseppemota@gmail.com>
2024-12-24 18:11:05 -0300

    splash and setting gradle.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce/util/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
--- lib/util/theme/custom_themes/bottom_sheet_theme.dart
+++ lib/util/theme/custom_themes/bottom_sheet_theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:e_commerce/util/constants/sizes.dart';
 import 'package:e_commerce/util/constants/colors.dart';
 
 class TBottomSheetTheme{
